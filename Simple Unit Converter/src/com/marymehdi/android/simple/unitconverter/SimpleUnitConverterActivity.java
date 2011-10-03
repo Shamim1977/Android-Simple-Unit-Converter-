f52bf7133c0c4b60673b028a119fde759db93d13
Mahdi Abdinejadi <mehdi61b@gmail.com>
2011-10-03 14:05:34 +0200

    Initial project with Eclipse template

---
+++ Simple Unit Converter/src/com/marymehdi/android/simple/unitconverter/SimpleUnitConverterActivity.java
@@ -0,0 +1,13 @@
+package com.marymehdi.android.simple.unitconverter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SimpleUnitConverterActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
